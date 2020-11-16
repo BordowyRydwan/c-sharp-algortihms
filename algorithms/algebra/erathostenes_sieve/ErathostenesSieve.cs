2d0cd129959b7aa14e1b7a9d54bc386a0af8687e
Pawluch <dawid.wijata@hotmail.com>
2020-11-16 19:59:27 +0100

    Add algebraic algorithms

---
+++ algorithms/algebra/erathostenes_sieve/ErathostenesSieve.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static partial class Algorithms
+{
+    public static List<int> FindPrimesLowerThanN(int n)
+    {
+        bool[] sieve = new bool[n];
+        List<int> primes = new List<int>();
+
+        for (int i = 2; i < sieve.Length; ++i)
+        {
+            sieve[i] = true;
+        }
+
+        for (int i = 2; i * i <= n; ++i)
+        {
+            if (sieve[i])
+            {
+                for (int j = i * i; j < n; j += i)
+                {
+                    sieve[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i < sieve.Length; ++i)
+        {
+            if (sieve[i])
+            {
+                primes.Add(i);
+            }
+        }
+
+        return primes;
+    }
+
+    public static bool IsPrime(int n)
+    {
+        if(n < 2)
+        {
+            return false;
+        }
+
+        for(int i = 2; i * i <= n; ++i)
+        {
+            if(n % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ algorithms/algebra/erathostenes_sieve/ErathostenesSieve_Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ErathostenesSieveTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public bool CompareLists(List<int> list1, List<int> list2)
+        {
+            if(list1.Count != list2.Count)
+            {
+                return false;
+            }
+
+            for(int i = 0; i < list1.Count; ++i)
+            {
+                if(list1[i] != list2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        [TestMethod]
+        public void FindPrimesLowerThanN_Correctness()
+        {
+            Tuple<List<int>, List<int>>[] testCases = {
+                new Tuple<List<int>, List<int>>(Algorithms.FindPrimesLowerThanN(7), new List<int>(){2, 3, 5}),
+                new Tuple<List<int>, List<int>>(Algorithms.FindPrimesLowerThanN(10), new List<int>(){2, 3, 5, 7}),
+                new Tuple<List<int>, List<int>>(Algorithms.FindPrimesLowerThanN(11), new List<int>(){2, 3, 5, 7}),
+                new Tuple<List<int>, List<int>>(Algorithms.FindPrimesLowerThanN(16), new List<int>(){2, 3, 5, 7, 11, 13}),
+                new Tuple<List<int>, List<int>>(Algorithms.FindPrimesLowerThanN(18), new List<int>(){2, 3, 5, 7, 11, 13, 17}),
+            };
+
+            foreach (Tuple<List<int>, List<int>> testCase in testCases)
+            {
+                Assert.AreEqual(true, CompareLists(testCase.Item1, testCase.Item2));
+            }
+        }
+
+        [TestMethod]
+        public void CheckIsPrime_Correctness()
+        {
+            Tuple<bool, bool>[] testCases = {
+                new Tuple<bool, bool>(Algorithms.IsPrime(7), true),
+                new Tuple<bool, bool>(Algorithms.IsPrime(21), false),
+                new Tuple<bool, bool>(Algorithms.IsPrime(12320), false),
+                new Tuple<bool, bool>(Algorithms.IsPrime(47), true),
+                new Tuple<bool, bool>(Algorithms.IsPrime(9), false),
+            };
+
+            foreach (Tuple<bool, bool> testCase in testCases)
+            {
+                Assert.AreEqual(testCase.Item2, testCase.Item1);
+            }
+        }
+    }
+}
